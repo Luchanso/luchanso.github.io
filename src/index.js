78080548a281b7ea45cb3ddf2c8b105e7a69dd8c
Oleg Loutchansky <luchansodev@gmail.com>
2018-08-18 11:24:08 +0300

    remove service worker

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
